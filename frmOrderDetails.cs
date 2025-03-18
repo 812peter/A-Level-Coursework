44b09871405fcb996d393f9f245ba36a21d8b5b6
Peter Ivanov <226264@live.brock.ac.uk>
2025-03-18 14:39:35 +0000

    74474747

--- frmOrderDetails.cs
+++ frmOrderDetails.cs
@@ -21,6 +21,7 @@ namespace Coursework
 
         bool chkCompletedEvent = false;
         int orderID;
+        List<string> products = new List<string>();
 
         internal void LoadDetails(string selectedOrderID)
         {
@@ -59,8 +60,24 @@ namespace Coursework
                 lstProducts.Items.Add(dr[0].ToString());
                 lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add((dr[1]).ToString());
                 lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add(dr[2].ToString());
-                lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add(dr[3].ToString());
-                lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add("£" + (Convert.ToDouble(dr[4]) * Convert.ToDouble(dr[0])));
+                string material = dr[3].ToString();
+                string materialShortVer = "";
+                lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add(material);
+                for (int i = 0; i < material.Length; i++)
+                {
+                    string original = material[i].ToString();
+                    string upper = material[i].ToString().ToUpper();
+                    if (original == upper && original != " ")
+                    {
+                        materialShortVer += original;
+                    }
+                }
+                double pricePP = Convert.ToDouble(dr[4]) * Convert.ToDouble(dr[0]);
+                lstProducts.Items[lstProducts.Items.Count - 1].SubItems.Add("£" + pricePP.ToString());
+                products.Add($"{(dr[1]).ToString().ToUpper()}{materialShortVer.ToLower()}{dr[2].ToString()}mm");
+                products.Add(dr[0].ToString());
+                products.Add($"£{pricePP.ToString()}");
+                //products.Add($"{(dr[1]).ToString().ToUpper()}{materialShortVer.ToLower()}{dr[2].ToString()}mm                              {dr[0].ToString()}         £{pricePP.ToString()}");
             }
 
             sqlStr = $"SELECT FirstName, Surname, Email, PhoneNumber, CompanyName FROM tblUser WHERE UserID = {userID}";
@@ -151,16 +168,37 @@ namespace Coursework
             int x;
             Font myFont1 = new Font("Cooper", 18, FontStyle.Bold);
             Font myFont2 = new Font("Times New Roman", 10, FontStyle.Bold);
-            Font myFont3 = new Font("Times New Roman", 10);
+            Font myFont3 = new Font("Times New Roman", 8);
             x = e.MarginBounds.X - 50;
             y = e.MarginBounds.Y - 70;
-            string str = "REINFORCEMENTS";
-            e.Graphics.DrawString(str, myFont1, Brushes.Black, x, y);
-            str = "    120 St Georges Ave, Poole, BH12 4ND\n                     0-7927-369-011";
-            y += 30;
-            e.Graphics.DrawString(str, myFont3, Brushes.Black, x, y);
-            y += 45;
+            e.Graphics.DrawString("REINFORCEMENTS", myFont1, Brushes.Black, x, y);
+            y += 35;
+            e.Graphics.DrawString("            120 St Georges Ave, Poole, BH12 4ND\n                             0-7927-369-011", myFont3, Brushes.Black, x, y);
+            y += 38;
             e.Graphics.DrawString("*****************************************************************", myFont3, Brushes.Black, 0, y);
+            y += 30;
+            e.Graphics.DrawString("NAME                               QTY         PRICE", myFont2, Brushes.Black, x, y);
+            y += 15;
+            int count = 0;
+            foreach (var product in products)
+            {
+                e.Graphics.DrawString(product, myFont3, Brushes.Black, x, y);
+                if (count % 2 == 0)
+                {
+                    y += 15;
+                    x += 70;
+                }
+                else if (count % 3 == 0)
+                {
+                    x = e.MarginBounds.X - 50;
+                    count = 0;
+                }
+                else
+                {
+                    x += 100;
+                }
+                count++;
+            }
         }
     }
 }
